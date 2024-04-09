b692d0ec0ad4c20269cab53f8fe2cf561fd7cdc5
Niall Lewin <Lewin91@hotmail.com>
2024-04-09 18:08:05 +0100

    Added overflow to tables

--- SchedulerPro.Web/src/components/meetingsTable/index.tsx
+++ SchedulerPro.Web/src/components/meetingsTable/index.tsx
@@ -38,45 +38,47 @@ const MeetingsTable: FC<meetingsTableProps> = ({ meetings, tzOverride }) => {
   return (
     <div className={styles.meetingsTableContainer}>
       <h3>Meetings</h3>
-      <table>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Meeting</th>
-            <th>Start</th>
-            <th>Duration</th>
-            <th>Attendees</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {meetings.map(({ id, name, startTime, endTime, attendees }) => {
-            const st = tzOverride ? moment.utc(startTime).tz(tzOverride) : moment.utc(startTime).local();
-            const et = tzOverride ? moment.utc(endTime).tz(tzOverride) : moment.utc(endTime).local();
-            const duration = et.diff(st, 'minutes');
-            return (
-              <tr key={id}>
-                <td>{st.format('LL')}</td>
-                <td>{name}</td>
-                <td>{st.format('LT')}</td>
-                <td>{duration} minutes</td>
-                <td>
-                  <ul>
-                    {attendees.map(({ id, firstName, lastName }) => (
-                      <li key={id}><a href={`/user/${id}`}>{firstName} {lastName}</a></li>
-                    ))}
-                  </ul>
-                </td>
-                <td className={styles.actionsContainer}>
-                  <button className={styles.deleteButton} onClick={() => handleDeletingMeeting(id)}>Remove meeting</button>
-                  {attendees.some(({ id }) => id === loggedInUser.id) &&
-                    <button className={styles.leaveButton} onClick={() => handleRemovingSelfFromMeeting(id)}>Leave meeting</button>}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className={styles.tableWrapper}>
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Meeting</th>
+              <th>Start</th>
+              <th>Duration</th>
+              <th>Attendees</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {meetings.map(({ id, name, startTime, endTime, attendees }) => {
+              const st = tzOverride ? moment.utc(startTime).tz(tzOverride) : moment.utc(startTime).local();
+              const et = tzOverride ? moment.utc(endTime).tz(tzOverride) : moment.utc(endTime).local();
+              const duration = et.diff(st, 'minutes');
+              return (
+                <tr key={id}>
+                  <td>{st.format('LL')}</td>
+                  <td>{name}</td>
+                  <td>{st.format('LT')}</td>
+                  <td>{duration} minutes</td>
+                  <td>
+                    <ul>
+                      {attendees.map(({ id, firstName, lastName }) => (
+                        <li key={id}><a href={`/user/${id}`}>{firstName} {lastName}</a></li>
+                      ))}
+                    </ul>
+                  </td>
+                  <td className={styles.actionsContainer}>
+                    <button className={styles.deleteButton} onClick={() => handleDeletingMeeting(id)}>Remove meeting</button>
+                    {attendees.some(({ id }) => id === loggedInUser.id) &&
+                      <button className={styles.leaveButton} onClick={() => handleRemovingSelfFromMeeting(id)}>Leave meeting</button>}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- SchedulerPro.Web/src/components/usersTable/index.tsx
+++ SchedulerPro.Web/src/components/usersTable/index.tsx
@@ -28,33 +28,35 @@ const UsersTable: FC<usersTableProps> = ({ users }) => {
   return (
     <div className={styles.usersTableContainer}>
       <h3>Users</h3>
-      <table>
-        <thead>
-          <tr>
-            <th>User</th>
-            <th>Timezone</th>
-            <th>Local time</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(({ id, firstName, lastName, timeZoneId }) => {
-            const localTime = moment.utc().tz(timeZoneId).format('LT');
-            return (
-              <tr key={id}>
-                <td><a href={`/user/${id}`}>{firstName} {lastName}</a></td>
-                <td>{timeZoneId}</td>
-                <td>{localTime}</td>
-                <td>
-                  {(loggedInUser.id === id) ?
-                    <p className={styles.cantDelete}>Can't delete self</p> :
-                    <button className={styles.deleteButton} onClick={() => handleDeleteUser(id)}>Delete user</button>}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className={styles.tableWrapper}>
+        <table>
+          <thead>
+            <tr>
+              <th>User</th>
+              <th>Timezone</th>
+              <th>Local time</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(({ id, firstName, lastName, timeZoneId }) => {
+              const localTime = moment.utc().tz(timeZoneId).format('LT');
+              return (
+                <tr key={id}>
+                  <td><a href={`/user/${id}`}>{firstName} {lastName}</a></td>
+                  <td>{timeZoneId}</td>
+                  <td>{localTime}</td>
+                  <td>
+                    {(loggedInUser.id === id) ?
+                      <p className={styles.cantDelete}>Can't delete self</p> :
+                      <button className={styles.deleteButton} onClick={() => handleDeleteUser(id)}>Delete user</button>}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div >
   )
 };
